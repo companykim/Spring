d69741a9db55c1d1c1cde7bd3c986d6bd832e4e7
김기업 <102665905+companykim@users.noreply.github.com>
2023-08-02 18:22:06 +0900

    Add files via upload

---
+++ jspSpring/src/main/java/www/dream/bb/serivce/SCalc.java
@@ -0,0 +1,25 @@
+package www.dream.bb.serivce;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class SCalc {
+	public static int calc(HttpServletRequest req) {
+		int operandFirst = Integer.parseInt(req.getParameter("operandFirst"));
+		String operator = req.getParameter("operator");
+		int operandSecond = Integer.parseInt(req.getParameter("operandSecond"));
+
+		int ret = 0;
+		switch (operator) {
+		case "+": ret = operandFirst + operandSecond;  
+			break;
+		case "-": ret = operandFirst - operandSecond; 
+			break;
+		case "*": ret = operandFirst * operandSecond; 
+			break;
+		case "/": 
+			ret =operandSecond == 0 ? Integer.MAX_VALUE : operandFirst / operandSecond; 
+			break;
+		}
+		return ret;
+	}
+}
