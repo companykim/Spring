f7cc79b2f0610a05cb278cef507c002ee36b2916
김기업 <102665905+companykim@users.noreply.github.com>
2023-08-03 17:26:18 +0900

    Delete visitorPattern directory

--- visitorPattern/src/www/dream/vp/model/Element.java
+++
@@ -1,9 +0,0 @@
-package www.dream.vp.model;
-
-import www.dream.vp.visitor.Visitor;
-
-public abstract class Element {
-	public abstract void accept(Visitor visitor);
-	public abstract String whoAreYou();
-	
-}
