b22be32008ea3601f00b286d1276e220992685a0
김기업 <102665905+companykim@users.noreply.github.com>
2023-08-03 17:27:32 +0900

    8/3 Visitor Pattern added UML

---
+++ visitorPattern/src/www/dream/vp/model/Element.java
@@ -0,0 +1,9 @@
+package www.dream.vp.model;
+
+import www.dream.vp.visitor.Visitor;
+
+public abstract class Element {
+	public abstract void accept(Visitor visitor);
+	public abstract String whoAreYou();
+	
+}
