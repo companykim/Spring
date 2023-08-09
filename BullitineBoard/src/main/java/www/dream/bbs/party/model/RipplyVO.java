b58039f5357980d76ccec3b734a397dfbef4ecc2
김기업 <102665905+companykim@users.noreply.github.com>
2023-08-09 10:36:41 +0900

    Delete RipplyVO.java

--- BullitineBoard/src/main/java/www/dream/bbs/party/model/RipplyVO.java
+++
@@ -1,8 +0,0 @@
-package www.dream.bbs.party.model;
-
-import www.dream.bbs.board.framework.model.MasterEntity;
-
-public class RipplyVO extends MasterEntity {
-	private PartyVO writer;
-	private String content;
-}
