e2ab9c5d16735bbf8ce72988ad463bbfc2add264
김기업 <102665905+companykim@users.noreply.github.com>
2023-08-09 10:18:34 +0900

    Delete BullitineBoard directory

--- BullitineBoard/src/main/java/www/dream/bbs/board/model/RipplyVO.java
+++
@@ -1,9 +0,0 @@
-package www.dream.bbs.board.model;
-
-import www.dream.bbs.board.framework.model.MasterEntity;
-import www.dream.bbs.party.model.PartyVO;
-
-public class RipplyVO extends MasterEntity {
-	private PartyVO writer; // 게시물 작성자
-	private String content; // 내용
-}
